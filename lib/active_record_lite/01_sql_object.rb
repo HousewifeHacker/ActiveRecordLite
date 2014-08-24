6170ef0d6088f1fa73876e5a9a6b7e350fdf970f
housewifehacker <housewifehacker@gmail.com>
2014-08-24 12:23:50 -0500

    Meta program SQL objects and associations belongs_to, has_many, and has_one through

--- lib/active_record_lite/01_sql_object.rb
+++ lib/active_record_lite/01_sql_object.rb
@@ -5,53 +5,108 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+   
+    query = DBConnection.execute2("SELECT * FROM #{table_name}")
+    query.first.map!(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |col|
+      define_method("#{col}=") do |value|
+        self.attributes[col] = value
+      end
+      define_method(col) do
+        self.attributes[col]
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.tableize
   end
 
   def self.all
-    # ...
+    db = DBConnection.execute(<<-SQL)
+    SELECT
+      #{table_name}.*
+    FROM
+      #{table_name}
+    SQL
+
+    parse_all(db)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map do |result|
+      self.new(result)
+    end
   end
 
   def self.find(id)
-    # ...
+    db = DBConnection.execute(<<-SQL, id)
+    SELECT
+      #{ table_name }.*
+    FROM
+      #{ table_name }
+    WHERE
+      #{ table_name }.id = ?
+    SQL
+
+    parse_all(db).first
   end
 
   def attributes
-    # ...
+    @attributes ||= {}        
   end
 
   def insert
-    # ...
+    columns = self.class.columns
+    
+    col_names = columns.join(', ')
+    question_marks = (['?'] * columns.count).join(', ')
+    DBConnection.execute(<<-SQL, attribute_values)
+    INSERT INTO
+      #{ self.class.table_name } (#{ col_names })
+    VALUES
+      (#{ question_marks })
+    SQL
+    
+    new_id = DBConnection.last_insert_row_id
+    self.send(:id=, new_id)
   end
 
-  def initialize
-    # ...
+  def initialize(params = {})
+    params.each do |attr_name, value|
+      sym_name = attr_name.to_sym
+      unless self.class.columns.include?(sym_name)
+        raise "unknown attribute '#{ sym_name }'"
+      end
+      send("#{ sym_name }=", value)
+    end
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 
   def update
-    # ...
+    set_row = self.class.columns
+              .map{ |col_name| "#{ col_name } = ?" }.join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE 
+        #{ self.class.table_name }
+      SET
+        #{ set_row }
+      WHERE
+        #{ self.class.table_name }.id = ?
+     SQL
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map{ |col| send(col) }    
   end
 end
--- lib/active_record_lite/03_associatable.rb
+++ lib/active_record_lite/03_associatable.rb
@@ -10,41 +10,63 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = { class_name: name.to_s.camelcase,
+                 foreign_key: "#{name}_id".to_sym,
+                 primary_key: :id
+    }
+    defaults.merge!(options)
+    defaults.each{ |k, v| send("#{k}=", v) }
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    # has_many have plural association names
+     defaults = { class_name: name.to_s.singularize.camelcase,
+                 foreign_key: "#{self_class_name.underscore}_id".to_sym,
+                 primary_key: :id
+    }
+    defaults.merge!(options)
+    defaults.each{ |k, v| self.send("#{k}=", v) }
   end
 end
 
 module Associatable
-  # Phase IVb
   def belongs_to(name, options = {})
-    # ...
+    assoc_options[name] = BelongsToOptions.new(name, options)
+    define_method(name) do
+      obj = self.class.assoc_options[name]
+      fk = self.send(obj.foreign_key)
+      obj.model_class.where(obj.primary_key => fk).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      obj = self.class.assoc_options[name]
+      pk = self.send(obj.primary_key)
+      obj.model_class.where(obj.foreign_key => pk)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase V. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
